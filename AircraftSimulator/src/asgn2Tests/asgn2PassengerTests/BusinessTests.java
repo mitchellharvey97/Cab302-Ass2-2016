7b89966615bfd89a343a626642000dd060ada244
Mitchell Harvey <Mitchell Harvey>
2016-05-15 01:37:38 +0100

    Done all specifc tests for Pass, but still more to go on the general passenger class test

--- AircraftSimulator/src/asgn2Tests/asgn2PassengerTests/BusinessTests.java
+++ AircraftSimulator/src/asgn2Tests/asgn2PassengerTests/BusinessTests.java
@@ -8,8 +8,12 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import asgn2Passengers.Business;
+import asgn2Passengers.First;
+import asgn2Passengers.Passenger;
+
 /**
- * @author Andrew
+ * @author Mitchell
  *
  */
 public class BusinessTests {
@@ -17,13 +21,29 @@ public class BusinessTests {
 	/**
 	 * @throws java.lang.Exception
 	 */
+	public static Business bus_pass;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		bus_pass = new Business(10, 102);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void checkpass_id() {
+		assertEquals("J:0", bus_pass.getPassID());
+	}
+
+	@Test
+	public void check_noSeatMsg() {
+		assertEquals("No seats available in Business", bus_pass.noSeatsMsg());
+	}
+
+	@Test
+	public void upgrade_pas() {
+		Passenger upgraded = bus_pass.upgrade();
+		assertTrue(upgraded instanceof First);
+		// NEED TO READ SPECS AS TO WHAT TO DO WITH THE PASSENGER ID HERE
+		assertEquals("F:0", upgraded.getPassID());
 	}
 
 }
